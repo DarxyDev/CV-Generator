a29c4375e9755635147ab9013172b7d60c7fd6df
DarxyDev <damongarvin@gmail.com>
2024-02-09 20:21:16 -0500

    added basic form/preview components and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './styles/reset.css'
+import './styles/App.css'
+import Preview from './components/Preview'
+import Form from './components/Form'
 
 function App() {
 
   return (
-    <>
-    Example Text.
-    </>
+    <div id='main-container'>
+      <Form />
+      <Preview />
+    </div>
   )
 }
 
---
+++ src/components/Preview.jsx
@@ -0,0 +1,17 @@
+import '../styles/Preview.css'
+export default function Preview(){
+    return (
+        <div id='preview' className = 'right-half main-section'>
+            <Page></Page>
+        </div>
+    )
+}
+
+function Page(){
+
+    return (
+        <div id='page'>
+
+        </div>
+    )
+}
